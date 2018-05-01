d76587f245e0040d0e4da5d54d8c436bd397e3a1
Samaritan <1594320575@qq.com>
2018-05-01 10:49:22 +0800

    Create 976D 度数集.cpp

---
+++ 976D 度数集.cpp
@@ -0,0 +1,30 @@
+/*
+ * @Samaritan_infi
+ */
+/// 给出一个无向图的度数集合
+/// 构造出图的边
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1000005;
+int a[maxn], f[maxn], x[maxn], y[maxn];
+int n, l, r, cnt;
+int main()
+{
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &a[i]);
+		f[a[i]] = true;
+	}
+	while (a[n] > r)
+	{
+		do {
+			for (int i = l + 2; i <= a[n] + 1; i++) x[cnt] = l + 1, y[cnt++] = i;
+			l++, r++;
+		}while (!f[r]);
+		n--;
+	}
+	printf("%d\n", cnt);
+	for (int i = 0; i < cnt; i++) printf("%d %d\n", x[i], y[i]);
+	return 0;
+}
